2791b24aae6eb1fd9af3a0d80dc735af125d1ff9
abhishekInvictus <abhishek@invictusdata.ai>
2025-04-09 17:34:46 +0530

    Business Health Score card image chnaged

--- src/subpages/BusinessHealth.tsx
+++ src/subpages/BusinessHealth.tsx
@@ -3,7 +3,7 @@ import { useTheme } from '../hooks/useTheme';
 import NavBar from '../components/NavBar';
 import Footer from '../components/Footer';
 import { CheckCircle2 } from 'lucide-react';
-import aiDrivenInsights from '../assets/features/PDPCompliance&ContentOptimization.png';
+import aiDrivenInsights from '../assets/features/BusinessHealthScorecard.png';
 
 const BusinessHealth: React.FC = () => {
   const { theme } = useTheme();
